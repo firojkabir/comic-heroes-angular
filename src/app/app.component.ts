115e5348c8def08850060aee517f44e6758b3211
Md Firoj Kabir <firoj.kabir65@gmail.com>
2023-04-29 16:01:53 +0200

    Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Comic Heroes';
+
+  constructor(private http: HttpClient) {
+    console.log(`${environment.apiBaseUrl}/todos`);
+  }
+
+  ngOnInit() {
+    console.log('oninit');
+    this.http.get(`${environment.apiBaseUrl}/todos`).subscribe({
+      next: (respone: any) => {
+        console.log(respone);
+      },
+    });
+  }
 }
